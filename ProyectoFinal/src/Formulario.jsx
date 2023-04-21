828374db93b61df80a70fd8819fb4688fe471545
Josue Lobo <josue.lobo.5@hotmail.es>
2023-04-21 03:04:49 -0600

    Formulario base

--- ProyectoFinal/src/Formulario.jsx
+++ ProyectoFinal/src/Formulario.jsx
@@ -1,9 +1,26 @@
 import React, { useState } from "react";
+import './Formulario.css'
 
 function Formulario(){
+    const [paciente, setPasciente]=useState([])
+    const [mascota, setMascota]=useState("")
+    const [dueño, setDueño]=useState("")
+
+    function agregarPaciente(){
+        setPasciente([...paciente,mascota])
+        setMascota("")
+        setDueño("")
+    }
 
     return(
         <div className="Formulario">
+            <form className="inputFormulario" action="">
+                <label htmlFor="">Nombre Mascota</label>
+                <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type="text" />
+                <label htmlFor="">Nombre Dueño</label>
+                <input onChange={(e)=>{setDueño(e.target.value)}} value={dueño} type="text" />
+                <button onClick={agregarPaciente} type="submit">Agregar</button>
+            </form>
         </div>
         
     )
