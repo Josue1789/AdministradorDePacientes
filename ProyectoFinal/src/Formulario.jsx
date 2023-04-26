ddb1ea496bf7b3499e6fa63d8b359ec146effd9b
Josue Lobo <josue.lobo.5@hotmail.es>
2023-04-25 19:12:11 -0600

    Formulario completo

--- ProyectoFinal/src/Formulario.jsx
+++ ProyectoFinal/src/Formulario.jsx
@@ -5,11 +5,17 @@ function Formulario(){
     const [paciente, setPasciente]=useState([])
     const [mascota, setMascota]=useState("")
     const [dueño, setDueño]=useState("")
+    const [fecha, setFecha]=useState("")
+    const [hora, setHora]=useState("")
+    const [coms, setComs]=useState("")
 
     function agregarPaciente(){
-        setPasciente([...paciente,mascota])
+        setPasciente([...paciente,mascota,dueño,fecha,hora,coms])
         setMascota("")
         setDueño("")
+        setFecha("")
+        setHora("")
+        setComs("")
     }
 
     return(
@@ -19,6 +25,12 @@ function Formulario(){
                 <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type="text" />
                 <label htmlFor="">Nombre Dueño</label>
                 <input onChange={(e)=>{setDueño(e.target.value)}} value={dueño} type="text" />
+                <label htmlFor="">Fecha de Ingreso</label>
+                <input onChange={(e)=>{setFecha(e.target.value)}} value={fecha} type="date" />
+                <label htmlFor="">Hora de Ingreso</label>
+                <input onChange={(e)=>{setHora(e.target.value)}} value={hora} type="time" />
+                <label htmlFor="">Comentarios adicionales</label>
+                <textarea onChange={(e)=>{setComs(e.target.value)}} value={coms} type="text" />
                 <button onClick={agregarPaciente} type="submit">Agregar</button>
             </form>
         </div>
--- ProyectoFinal/src/Header.jsx
+++ ProyectoFinal/src/Header.jsx
@@ -4,6 +4,7 @@ function Header(){
 
     return(
         <div className="Header">
+            <h1>Esto es un titulo</h1>
         </div>
         
     )
