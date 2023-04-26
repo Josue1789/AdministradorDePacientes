0d6a01133eb87c990e9927890509a6a0a0c1d3b3
Josue Lobo <josue.lobo.5@hotmail.es>
2023-04-25 21:49:32 -0600

    Correcion de errores en funcionamiento del formulario

--- ProyectoFinal/src/Formulario.jsx
+++ ProyectoFinal/src/Formulario.jsx
@@ -1,40 +1,46 @@
 import React, { useState } from "react";
 import './Formulario.css'
 
-function Formulario(){
-    const [paciente, setPasciente]=useState([])
-    const [mascota, setMascota]=useState("")
-    const [dueño, setDueño]=useState("")
-    const [fecha, setFecha]=useState("")
-    const [hora, setHora]=useState("")
-    const [coms, setComs]=useState("")
+    function Formulario() {
+        const [pacientes, setPacientes] = useState([])
+        const [mascota, setMascota] = useState("")
+        const [dueño, setDueño] = useState("")
+        const [fecha, setFecha]=useState("")
+        const [hora, setHora]=useState("")
+        const [coms, setComs]=useState("")
 
     function agregarPaciente(){
-        setPasciente([...paciente,mascota,dueño,fecha,hora,coms])
+        const informacion={
+            mascota,
+            dueño,
+            fecha,
+            hora,
+            coms
+        }
+        setPacientes([...pacientes,informacion])
         setMascota("")
         setDueño("")
         setFecha("")
         setHora("")
         setComs("")
-    }
+}
 
-    return(
-        <div className="Formulario">
+    return (
+        <div>
             <form className="inputFormulario" action="">
-                <label htmlFor="">Nombre Mascota</label>
-                <input onChange={(e)=>{setMascota(e.target.value)}} value={mascota} type="text" />
+                <label htmlFor="mascota">Nombre Mascota </label>
+                <input id="mascota" onChange={(e) => { setMascota(e.target.value) }} value={mascota} type="text" />
                 <label htmlFor="">Nombre Dueño</label>
-                <input onChange={(e)=>{setDueño(e.target.value)}} value={dueño} type="text" />
+                <input onChange={(e) => { setDueño(e.target.value) }} value={dueño} type="text" />
                 <label htmlFor="">Fecha de Ingreso</label>
                 <input onChange={(e)=>{setFecha(e.target.value)}} value={fecha} type="date" />
                 <label htmlFor="">Hora de Ingreso</label>
                 <input onChange={(e)=>{setHora(e.target.value)}} value={hora} type="time" />
                 <label htmlFor="">Comentarios adicionales</label>
                 <textarea onChange={(e)=>{setComs(e.target.value)}} value={coms} type="text" />
-                <button onClick={agregarPaciente} type="submit">Agregar</button>
+                <button onClick={agregarPaciente} type='button'>Agregar</button>
             </form>
         </div>
-        
     )
 }
 
